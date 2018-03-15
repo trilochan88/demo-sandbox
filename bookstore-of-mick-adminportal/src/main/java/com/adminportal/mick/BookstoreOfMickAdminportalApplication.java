aa53ddd9de849830dccb47330477d60c7e4754f1
trilochan88 <trilochan88@gmail.com>
2018-03-15 14:54:27 +0530

    adding bookstore admin portal project

---
+++ bookstore-of-mick-adminportal/src/main/java/com/adminportal/mick/BookstoreOfMickAdminportalApplication.java
@@ -0,0 +1,12 @@
+package com.adminportal.mick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreOfMickAdminportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreOfMickAdminportalApplication.class, args);
+	}
+}
